cb2c3415b2d2ffda3cbf3408f7b9ac5a2181a387
Luiz Felipe Gonçalves Claudino <104796722+luiztw@users.noreply.github.com>
2022-06-08 09:17:45 -0300

    Add files via upload

---
+++ exerci3.java
@@ -0,0 +1,31 @@
+package EXERC3;
+
+ import java.text.DecimalFormat;
+import java.util.Scanner;
+public class exerci3 {
+ 
+    public static void main(String[] args) {
+        
+        Scanner ler = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+        int a,b;
+        Double c,d;
+
+        a = 3 - 2 - 1 + 2 + 1 + 3;
+        System.out.println("O valor dessa equação é " + a);
+
+        b = (2 * 3) - (4 * 5);
+        System.out.println("O valor dessa equação é " + b);
+
+        c = 2 + 6 - ((3.0/7.0) * 9);
+        System.out.println("O valor dessa equação é " + df.format(c));
+
+        d = 3.0 % (4.0 - 8.0);
+        System.out.println("O valor dessa equação é " + d);
+
+
+
+
+
+    }
+}
