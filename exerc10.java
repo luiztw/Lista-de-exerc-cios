6145b1cf39da9acc1b7dc869a59fa127feb09a99
Luiz Felipe Gonçalves Claudino <104796722+luiztw@users.noreply.github.com>
2022-07-13 11:04:06 -0300

    Add files via upload

---
+++ exerc10.java
@@ -0,0 +1,50 @@
+import javax.swing.JOptionPane;
+import javax.swing.*;
+
+public class exerc10 {
+
+    public static void main(String[] args) {
+
+        String dia = JOptionPane.showMessageDialog(null,"Digite número entre 1 e 7");
+
+        if (dia != null) {
+            try {
+
+                int diadasemana = Integer.parseInt(dia);
+
+                if (diadasemana == 1)
+                    dia = "Domingp";
+
+                else if (diadasemana == 2)
+                    dia = "Segunda";
+
+                else if (diadasemana == 3)
+                    dia = "Tercça";
+
+                else if (diadasemana == 4)
+                    dia = "Quarta";
+
+                else if (diadasemana == 5)
+                    dia = "Quinta";
+
+                else if (diadasemana == 6)
+                    dia = "Sexta";
+
+                else if (diadasemana == 7)
+                    dia = "Sabado";
+
+                else
+                    dia = "Desconheço";
+                JOptionPane.showMessageDialog(null, dia);
+
+            }
+
+            catch (NumberFormatException erro) {
+                JOptionPane.showMessageDialog(null, "Formato inválido");
+            }
+
+        }
+
+    }
+
+}
